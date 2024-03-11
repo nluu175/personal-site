af3fda4598783aa5cb0aea0fd75a452754dff083
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2024-03-10 21:17:37 -0600

    Edit info text

--- src/data/data.js
+++ src/data/data.js
@@ -2,13 +2,14 @@ export const mainInfo = [
   {
     title: "Welcome",
     content:
-      "Hello! Welcome to my personal website. My name is Minh and I'm currently a Junior Software Developer.",
+      "Hello! Welcome to my personal website. My name is Minh and I'm currently a Junior Software Developer. " +
+      "I made this page using React and spend several hours getting all the css stylings to work (CSS is a pain lol!!!).",
   },
   {
     title: "Bio",
     content:
-      "I'm an international student from Vietnam and I have recently graduated from the University of Alberta with a major in Computer Science. " +
-      "I'm currently looking for a position in Software Development/Engineering. " +
+      "I'm an international student from Vietnam and I have recently graduated from the University of Alberta, Canada with a major in Computer Science. " +
+      "I'm pursuing a career in Tech and currently looking for a position in Software Development/Engineering. " +
       "I do have experience with various languages and frameworks but my current focus is on Python and JavaScript.",
   },
   {
@@ -23,6 +24,13 @@ export const mainInfo = [
       "I had experience working as a full-stack developer in my past internships. " +
       "The most recent experience I had was with Natural Resources Canada as an Software Developer Intern. " +
       "Please refer to my LinkedIn for more information. ",
+  },
+  {
+    title: "Hobby",
+    content:
+      "I usually enjoy playing video games (Yes!! I've been gaming for my whole life) when I'm not on my favourite IDE debugging my code. " +
+      "Other than that, I do love listening to music and cooking as well. " +
+      "Hit me up if we share the same interest or if you want to hire me (hehe!)",
   },
 ];
 
